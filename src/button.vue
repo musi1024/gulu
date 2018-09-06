92d4cab5ffae52eb951b3367b7a98fbc6392ed91
musi <957028613@qq.com>
2018-09-07 00:30:27 +0800

    调整样式

--- src/button.vue
+++ src/button.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class='g-button' :class="{[`icon-${iconPosition}`]: true}" aria-hidden="true">
+    <button class='g-button' :class="{[`icon-${iconPosition}`]: true}">
         <svg v-if="icon" class="icon">
             <use :xlink:href="`#i-${icon}`"></use>
         </svg>
@@ -23,22 +23,23 @@
     }
 </script>
 <style lang="scss">
-    .g-button {
-        font-size: var(--font-size);
+    .g-button { 
+        font-size: var(--font-size); 
         height: var(--button-height);
-        padding: 0 1em;
+        padding: 0 1em; 
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
-        display: inline-flex;
-        justify-content: center;
+        display: inline-flex; 
+        justify-content: center; 
         align-items: center;
         vertical-align: middle;
         &:hover { border-color: var(--border-color-hover); }
         &:active { background: var(--button-active-bg); }
         &:focus { outline: none; }
+        > .content  { order: 2; }
         > .icon { order: 1; margin-right: .1em; }
-        > .content  { order: 2 }
+       
         &.icon-right {
             > .content { order: 1;}
             > .icon { order: 2; margin-right: 0; margin-left: .1em; }
