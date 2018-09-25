77e8734c67725b1ead0e1a0b5a0b28925c17fa60
musi <957028613@qq.com>
2018-09-26 00:33:52 +0800

    微重构col

--- src/col.vue
+++ src/col.vue
@@ -28,16 +28,29 @@
             pc: {type: Object, validator,},
             widePc: {type: Object, validator,}
         },
+        methods: {
+            createClasses(obj, str = '') {
+                if (!obj) { return [] }
+                let array = []
+                if (obj.span) {
+                    array.push(`col-${str}${obj.span}`)
+                }
+                if (obj.offset) {
+                    array.push(`offset-${str}${obj.offset}`)
+                }
+                return array
+            }
+        },
         computed: {
             colClass () {
                 let {span, offset, ipad, narrowPc, pc, widePc} = this
+                let createClasses = this.createClasses
                 return [
-                    span && `col-${span}`,
-                    offset && `offset-${offset}`,
-                    ... (ipad ? [`col-ipad-${ipad.span}`] : []),
-                    ... (narrowPc ? [`col-narrow-pc-${narrowPc.span}`] : []),
-                    ... (pc ? [`col-pc-${pc.span}`] : []),
-                    ... (widePc ? [`col-wide-pc-${widePc.span}`] : []),
+                    ...createClasses({span, offset}),
+                    ...createClasses(ipad, 'ipad-'),
+                    ...createClasses(narrowPc, 'narrow-pc-'),
+                    ...createClasses(pc, 'pc-'),
+                    ...createClasses(widePc, 'wide-pc-'),
                 ]
             },
             colStyle () {
