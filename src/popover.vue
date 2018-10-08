f705c5c21d035577189fe05f2950bc4d321d93ec
musi <957028613@qq.com>
2018-10-09 01:39:48 +0800

    解决组件父元素overhidden 的bug

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="popover" @click="clickButton">
-        <div class="content-wrapper" v-if="visible" ref='content'>
+        <div class="content-wrapper" v-if="visible" ref='contentWrapper'>
             <slot name="content"></slot>
         </div>
-        <span class="button-wrapper" ref="button">
+        <span ref="triggerWrapper">
             <slot></slot>
         </span>
     </div>
@@ -19,11 +19,15 @@
         },
         methods: {
             clickButton(e) {
-                if (this.$refs.button.contains(e.target)) {
+                if (this.$refs.triggerWrapper.contains(e.target)) {
                     this.visible = !this.visible
                     if (this.visible) {
                         console.log('click button')
                         this.$nextTick(() => {
+                            document.body.appendChild(this.$refs.contentWrapper)
+                            let { width, height, top, left } = this.$refs.triggerWrapper.getBoundingClientRect()
+                            this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+                            this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
                             document.addEventListener('click', this.clickOther)
                         })
                     } else {
@@ -32,7 +36,7 @@
                 }
             },
             clickOther(e) {
-                if (!this.$refs.content.contains(e.target)) {
+                if (!this.$refs.contentWrapper.contains(e.target)) {
                     this.visible = !this.visible
                     console.log('doc click');
                     document.removeEventListener('click', this.clickOther)
@@ -46,13 +50,11 @@
         display: inline-block;
         vertical-align: top;
         position: relative;
-
-        .content-wrapper {
-            position: absolute;
-            bottom: 100%;
-            left: 0;
-            border: 1px solid red;
-            box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-        }
+    }
+    .content-wrapper {
+        position: absolute;
+        border: 1px solid red;
+        box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+        transform: translateY(-100%);
     }
 </style>
