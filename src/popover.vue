a1c8aa7bb236d1129fb085f2b1e1051acc9a5b7e
musi <957028613@qq.com>
2018-10-09 01:22:49 +0800

    修改样式

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="popover" @click="clickButton">
-        <div class="content-warpper" v-if="visible" ref='content'>
+        <div class="content-wrapper" v-if="visible" ref='content'>
             <slot name="content"></slot>
         </div>
-        <span class="button-warpper" ref="button">
+        <span class="button-wrapper" ref="button">
             <slot></slot>
         </span>
     </div>
@@ -20,21 +20,21 @@
         methods: {
             clickButton(e) {
                 if (this.$refs.button.contains(e.target)) {
-                    this.visible = !this.visible 
-                    if (this.visible) {       
+                    this.visible = !this.visible
+                    if (this.visible) {
                         console.log('click button')
                         this.$nextTick(() => {
                             document.addEventListener('click', this.clickOther)
-                        }) 
+                        })
                     } else {
                         document.removeEventListener('click', this.clickOther)
-                    }   
+                    }
                 }
             },
             clickOther(e) {
                 if (!this.$refs.content.contains(e.target)) {
                     this.visible = !this.visible
-                    console.log('doc click');   
+                    console.log('doc click');
                     document.removeEventListener('click', this.clickOther)
                 }
             }
@@ -49,9 +49,10 @@
 
         .content-wrapper {
             position: absolute;
+            bottom: 100%;
+            left: 0;
             border: 1px solid red;
             box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-            transform: translateY(-100%);
         }
     }
 </style>
