5c99e9dabdfbd5271d98ad70cf30cf7742584a53
musi <957028613@qq.com>
2018-10-10 01:24:25 +0800

    添加插槽 api

--- src/popover.vue
+++ src/popover.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="popover" ref="popover">
         <div class="content-wrapper" v-if="visible" ref='contentWrapper' :class="{[`position-${position}`]: true}">
-            <slot name="content"></slot>
+            <slot name="content" :close="closePopover"></slot>
         </div>
         <span ref="triggerWrapper">
             <slot></slot>
