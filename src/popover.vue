bfbb7a9c71c1e12a5768a95b3a8a89bc40528aea
musi <957028613@qq.com>
2018-10-09 22:57:00 +0800

    添加基本样式

--- src/popover.vue
+++ src/popover.vue
@@ -30,12 +30,12 @@
                 this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
             },
             clickButton(e) {
-                if (this.$refs.triggerWrapper.contains(e.target)) { 
-                    this.visible = !this.visible               
-                    if (this.visible) {        
-                        this.openPopover()        
+                if (this.$refs.triggerWrapper.contains(e.target)) {
+                    this.visible = !this.visible
+                    if (this.visible) {
+                        this.openPopover()
                     } else {
-                        this.closePopover(e)  
+                        this.closePopover(e)
                     }
                 }
             },
@@ -49,12 +49,15 @@
                 if (!this.$refs.contentWrapper.contains(e.target)) {
                     this.visible = false
                     document.removeEventListener('click', this.closePopover)
-                }  
+                }
             }
         }
     }
 </script>
 <style lang='scss' scoped>
+    $border-color: #333;
+    $border-radius: 4px;
+
     .popover {
         display: inline-block;
         vertical-align: top;
@@ -63,8 +66,35 @@
 
     .content-wrapper {
         position: absolute;
-        border: 1px solid red;
-        box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+        border: 1px solid $border-color;
+        border-radius: $border-radius;
+        filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.5));
+        background: white;
         transform: translateY(-100%);
+        margin-top: -15px;
+        padding: .5em 1em;
+        max-width: 20em;
+        word-break: break-all;
+
+        &::before,
+        &::after {
+            content: '';
+            display: block;
+            border: 10px solid transparent;
+            width: 0;
+            height: 0;
+            position: absolute;
+            left: 10px;
+        }
+
+        &::before {
+            border-top-color: black;
+            top: 100%;
+        }
+
+        &::after {
+            border-top-color: white;
+            top: calc(100% - 1px);
+        }
     }
 </style>
