5a4d00fe4ed5c52bb83088b5fc7c7a432ad59fdd
musi <957028613@qq.com>
2018-10-10 22:40:03 +0800

    selected 变为 array

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -28,20 +28,20 @@
         },
         inject: ['eventBus'],
         mounted() {
-            this.eventBus && this.eventBus.$on('update:selected', (name) => {
-                if (name !== this.name) {
-                    this.open = false
-                } else {
+            this.eventBus && this.eventBus.$on('update:selected', (names) => {
+                if (names.indexOf(this.name) >= 0 ) {
                     this.open = true
+                } else {
+                    this.open = false
                 }
             })
         },
         methods: {
             toggle() {
                 if (this.open) {
-                    this.open = false
+                    this.eventBus && this.eventBus.$emit('update:removeSelected', this.name)
                 } else {
-                    this.eventBus && this.eventBus.$emit('update:selected', this.name)
+                    this.eventBus && this.eventBus.$emit('update:addSelected', this.name)
                 }
             },
         },
