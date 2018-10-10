8ef5ea8c63c519b10654cc11321fb736ac77795e
musi <957028613@qq.com>
2018-10-10 22:10:32 +0800

    添加 single 和 select

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="collapseItem">
-        <div class="title" @click="open=!open">
+        <div class="title" @click="toggle">
             {{title}}
         </div>
         <div class="content" v-if="open">
@@ -15,13 +15,36 @@
             title: {
                 type: String,
                 required: true
+            },
+            name: {
+                type: String,
+                required: true
             }
         },
         data() {
             return {
                 open: false
             }
-        }
+        },
+        inject: ['eventBus'],
+        mounted() {
+            this.eventBus && this.eventBus.$on('update:selected', (name) => {
+                if (name !== this.name) {
+                    this.open = false
+                } else {
+                    this.open = true
+                }
+            })
+        },
+        methods: {
+            toggle() {
+                if (this.open) {
+                    this.open = false
+                } else {
+                    this.eventBus && this.eventBus.$emit('update:selected', this.name)
+                }
+            },
+        },
     }
 </script>
 <style scoped lang="scss">
@@ -29,7 +52,7 @@
     $border-radius: 4px;
 
     .collapseItem {
-        > .title {
+        >.title {
             border: 1px solid $grey;
             margin-top: -1px;
             margin-left: -1px;
@@ -41,14 +64,14 @@
         }
 
         &:first-child {
-            > .title {
+            >.title {
                 border-top-left-radius: $border-radius;
                 border-top-right-radius: $border-radius;
             }
         }
 
         &:last-child {
-            > .title:last-child {
+            >.title:last-child {
                 margin-bottom: -1px;
                 border-bottom-left-radius: $border-radius;
                 border-bottom-right-radius: $border-radius;
--- src/collapse.vue
+++ src/collapse.vue
@@ -4,8 +4,35 @@
     </div>
 </template>
 <script>
+    import Vue from 'vue'
     export default {
-        name: "GuluCollapse"
+        name: "GuluCollapse",
+        data() {
+            return {
+                eventBus: new Vue()
+            }
+        },
+        provide() {
+            return {
+                eventBus: this.eventBus
+            }
+        },
+        props: {
+            single: {
+                type: Boolean,
+                default: false
+            },
+            selected: {
+                type: String
+            }
+        },
+        mounted() {
+            this.eventBus.$emit('update:selected', this.selected)
+            console.log(this.selected)
+            this.eventBus.$on('update:selected', (name) => {
+                this.$emit('update:selected', name)
+            })
+        }
     }
 </script>
 <style scoped lang="scss">
